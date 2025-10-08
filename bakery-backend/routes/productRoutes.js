8cb39e2db6a8989f1218e82e7ff7948200dd0197
Hiral Prajapati <hiral@example.com>
2025-10-08 14:43:56 +0530

    set path

--- bakery-backend/routes/productRoutes.js
+++ bakery-backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Sample route
-router.get('/', (req, res) => {
+router.get('/product', (req, res) => {
   res.send('Product route is working!');
 });
 
