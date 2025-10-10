852598f9f2e5ccf0cdb83befd0e7fe274d243a7e
demo <v.sonali6545@gmail.com>
2025-10-10 18:11:24 +0530

    admin routes

--- admin_panel/src/App.jsx
+++ admin_panel/src/App.jsx
@@ -5,19 +5,7 @@ import AppRoutes from "./routes/AppRoutes";
 export default function App() {
   return (
     <BrowserRouter>
-
       <AppRoutes />
-
-      <Routes>
-        <Route path="/" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="products" element={<Products />} />
-          <Route path="customers" element={<Customers />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-      </Routes>
-
     </BrowserRouter>
   );
 }
